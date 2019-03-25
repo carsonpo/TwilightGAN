11785fc8b61e4ccc75e4c0187217bfec0a740925
carsonpo <38706134+carsonpo@users.noreply.github.com>
2019-03-25 16:56:38 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,5 +5,4 @@ pip install torchvision
 pip install dominate
 pip install visdom
 python train.py --dataroot ./train_data/train_data1 --name twilight_cyclegan --model cycle_gan --resize_or_crop scale_width_and_crop --loadSize 1024 --fineSize 360 --display_id 0 --checkpoints_dir /storage/chk_twlight
-zip -r  /artifacts/checkpoints.zip /storage/checkpoints
 
