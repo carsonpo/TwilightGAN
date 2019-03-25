6de25bd410c41aa3aa29cf12dc46d58eb75a1065
carsonpo <38706134+carsonpo@users.noreply.github.com>
2019-03-25 16:47:30 -0400

    Update train.py

--- train.py
+++ train.py
@@ -15,8 +15,9 @@ if __name__ == '__main__':
     model.setup(opt)
     visualizer = Visualizer(opt)
     total_steps = 0
+    opt.epoch_count = 300
 
-    for epoch in range(opt.epoch_count+100, opt.niter + opt.niter_decay + 1):
+    for epoch in range(opt.epoch_count, opt.niter + opt.niter_decay + 1):
         epoch_start_time = time.time()
         iter_data_time = time.time()
         epoch_iter = 0
